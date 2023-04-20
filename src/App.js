f0ebfdbd0be03b42520bf356c7bad57e7b719311
Ganesh Utla <ganeshutla888@gmail.com>
2023-04-20 23:35:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [employees, setEmployees] = useState([]);
+
+  const fetchEmployees = () => {
+    fetch("https://crud-employee-grp4.cyclic.app/getEmp")
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+      setEmployees(data);
+    })
+    .catch((e) => {
+      console.error(e);
+    })
+  }
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='emp-div'>
+        <h2>List Of Employees</h2>
+        <div className='emp-list'>
+          {employees.length > 0 && employees.map((item, index) => (
+            <div key={index} className='emp-item'>
+              <div className='emp-img'><img src='acc-img.png' alt='acc' /></div>
+              <div className='emp-details'>
+                <div><b>Full Name:</b> {item.FIRST_NAME} {item.LAST_NAME}</div>
+                <div><b>Gender:</b> {item.GENDER}</div>
+                <div><b>Email:</b> {item.EMAIL}</div>
+                <div><b>Phone:</b> {item.PHONE_NUMBER}</div>
+                <div><b>Salary:</b> {item.SALARY}</div>
+                <div><b>Location:</b> {item.CITY}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {employees.length===0 && <div>No Employee records found...</div>}
+      </div>
     </div>
   );
 }
